db4478caa706c000ca705cb902d7b0e51d5c1281
JKC <JKC@192.168.2.44>
2019-05-28 20:43:09 -0400

    base class with desired capabilities added

--- src/base.java
+++ src/base.java
@@ -1,9 +1,24 @@
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
 
 public class base {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MalformedURLException {
 		// TODO Auto-generated method stub
 
-	}
-
+		File appDir = new File("src");
+	     File app = new File(appDir, "ApiDemos-debug.apk");
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Xperia");
+		cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+		
+		AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"),cap);
+}
 }
